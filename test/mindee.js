af2f5c7f7c4cef2a428bd1edd528fa46fd3720e2
Frédéric Harper <hi@fred.dev>
2022-04-29 10:42:30 -0400

    fix the test

--- test/mindee.js
+++ test/mindee.js
@@ -8,7 +8,7 @@ require('should');
 
 const zapier = require('zapier-platform-core');
 
-const App = require('../../index');
+const App = require('../index');
 const appTester = zapier.createAppTester(App);
 
 describe('Create - Mindee_invoice', () => {
