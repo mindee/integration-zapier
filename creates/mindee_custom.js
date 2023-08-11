e5187ba89d1a8275362327f9339e029e7934ca74
xaviermindee <98530984+xaviermindee@users.noreply.github.com>
2023-08-11 14:18:33 +0200

    remove lowercase (#57)

--- creates/mindee_custom.js
+++ creates/mindee_custom.js
@@ -30,7 +30,6 @@ module.exports = {
     perform: (z, bundle) => {
       const apiVersion = bundle.inputData.api_version ? bundle.inputData.api_version : 1;
       const splitUrl = bundle.inputData.api_url
-            .toLowerCase()
             .replace("https://platform.mindee.com/","")
             .replace("https://api.mindee.net/v1/products/","")
             .split('/');
